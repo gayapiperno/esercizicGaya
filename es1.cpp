1fad30193b45a3b7722aa0cbcef35205259e3418
gayapiperno <stud.pipernoga@gdromagnosi.edu.it>
2026-03-05 08:00:54 +0000

    sggsgs

---
+++ es1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a, b;
+cout << "Inserisci due numeri: ";
+cin >> a >> b;
+int somma = a + b;
+cout << "La somma è: " << somma << endl;
+}
---
+++ es2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a, b;
+cout << "Inserisci due numeri: ";
+
+cin >> a >> b;
+if(a>0&&b>0){
+
+int area = a * b;
+cout << "Larea è: " << area << endl;
+}
+}
